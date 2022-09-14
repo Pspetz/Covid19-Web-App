d148da3801e421bac299605849bf91f7692ce5dd
Panagiotis Spentzaris <73785089+Pspetz@users.noreply.github.com>
2022-09-15 02:47:27 +0300

    Add files via upload

--- login-signup/scripts/epafi.js
+++ login-signup/scripts/epafi.js
@@ -18,14 +18,20 @@ async function epafi() {
     var db_response = await fetch('/epafi', options);
     var db_data = await db_response.json();
     console.log(db_data);
-    if(db_data.status === 'den uparxei epafi me krousma') {
-    alert(db_data.status)
+    if(db_data.status===undefined) {
+    alert('exete erthei se epafi me krousma')
+    for(i in db_data){
+        $("<tr><td>" + i +"</td><td>" +  db_data[i].username+  "</td><td>"+ db_data[i].magazi + "</td><td>"+ new Date(db_data[i].pote).toISOString().replace("T", " ").slice(0, 19)+ "</td></tr>").appendTo("#myepafi");
+       }
+    }else{
+        alert(db_data.status)
+        for(i in db_data){
+            $("<tr><td>" + i +"</td><td>" +  db_data[i].username+  "</td><td>"+ db_data[i].magazi + "</td><td>"+ new Date(db_data[i].pote).toISOString().replace("T", " ").slice(0, 19)+ "</td></tr>").appendTo("#myepafi");
+           }
+    }
+    
 }
 
-for(i in db_data){
-    $("<tr><td>" + i +"</td><td>" +  db_data[i].username+  "</td><td>"+ db_data[i].magazi + "</td><td>"+ new Date(db_data[i].pote).toISOString().replace("T", " ").slice(0, 19)+ "</td></tr>").appendTo("#myepafi");
-   }
-}
 
 
 showbut2.onclick=epafi;
