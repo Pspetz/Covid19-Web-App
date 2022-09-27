561e863c4975a87e7337a3fee3892e5c7d8c7f94
Panagiotis Spentzaris <73785089+Pspetz@users.noreply.github.com>
2022-09-27 20:26:12 +0300

    Add files via upload

--- login-signup/scripts/uploadfile.js
+++ login-signup/scripts/uploadfile.js
@@ -3,7 +3,8 @@ var k = document.getElementById("eisagwgh");
 let uploadValues = {};
 data=new Array();
 
-function readJson() {
+
+function uploadfile() {
 
     let uploadValues = {};
     var jsonFile = document.getElementById("formFileLg").files[0];
@@ -11,43 +12,44 @@ function readJson() {
         var reader = new FileReader();
         reader.readAsText(jsonFile, "UTF-8");
         reader.onload = function () {
-            jsonContent = JSON.parse(reader.result);
-            console.log(jsonContent)
-            jsonEntry = jsonContent.entries;
-            console.log(jsonEntry)
-            for (var key in jsonEntry) {
-        
-               var valueArr = {
-                    "id": jsonEntry[key]["id"],
-                    "name": jsonEntry[key]["name"],
-                    "address": jsonEntry[key]["address"],
-                    "type": jsonEntry[key]["types"],
-                    "lat": jsonEntry[key].coordinates["lat"][0],
-                    "lng": jsonEntry[key].coordinates["lng"][1], 
+            JsonEnt = JSON.parse(reader.result);
+
+            for (var key in JsonEnt) {
+       
+
+                valueArr = {
+                    "type":JsonEnt[key]['types'][0],
+                    "id": JsonEnt[key]["id"],
+                    "name": JsonEnt[key]["name"],
+                    "address": JsonEnt[key]["address"],
+                    "lat": JsonEnt[key].coordinates.lat,
+                    "lng": JsonEnt[key].coordinates.lng,
                 };
 
-        
+                
                 uploadValues[key] = valueArr;
-                data.push(valueArr);
-                console.log(valueArr)
             }
             fetch("/upload",
             {
                 method: 'POST',
                 headers: { "Content-Type": "application/json"},
-                body: JSON.stringify(valueArr)
+                body: JSON.stringify(uploadValues)
             })
             .then(res => res.json())
             .then(data =>{
-                if(Object.keys(data).length >0 )
+                if(data.status==='The .json file is damaged')
                 {
-
-                alert('sucess')             
-   }
+                 alert("something went wrong!")
+                }
                    
-               
+                else 
+                {
+               alert("successfull upload!")
+                }
             })
         }
+
     }
 }
-k.onclick=readJson;
+
+k.onclick = uploadfile; 
